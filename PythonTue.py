212b0d9082431f09f49fdf549cd809bb5e347914
jonas310325 <43399487+jonas310325@users.noreply.github.com>
2018-12-17 14:31:38 +0800

    Add files via upload

--- PythonTue.py
+++ PythonTue.py
@@ -9,25 +9,26 @@ bg_END = pygame.image.load('sky_END.jpg')
 tux = pygame.image.load("Snipe.png")
 pygame.display.set_caption("First Game")
 Snipe= pygame.mixer.Sound("beam 8 small laser.ogg")
-posx=randint(0,800)
-posy=randint(0,850)
+posX=randint(0,800)
+posY=randint(0,850)
 score = 0
 timecounter= 120
 clock = pygame.time.Clock()
 texttime = '120'
-pygame.time.set_timer(pygame.USEREVENT, 1000) 
+pygame.time.set_timer(pygame.USEREVENT, 250)
+movecount=0
 music = pygame.mixer.music.load('M2U X NICODE - Lune.mp3')
 pygame.mixer.music.play(-1)
 while True:
-	clock.tick(60)
-	screen.blit(bg, (0,0))
+	clock.tick(30)
 	
 	
 	for x in pygame.event.get():
-		
+		prey = [pygame.image.load('bird_1.png'), pygame.image.load('bird_2.png'), pygame.image.load('bird_3.png'), pygame.image.load('bird_2.png')]
+
 		mx,my=pygame.mouse.get_pos()
-		prey_Y=pygame.image.load("Yellobird.png")
-		screen.blit(prey_Y,(posx,posy))
+		screen.blit(bg, (0,0))
+		screen.blit(prey[movecount], (posX,posY))
 		screen.blit(tux,(mx-1007,my-1000))
 		fontscore = pygame.font.SysFont('cconsolas', 30, True)
 		fomttime = pygame.font.SysFont('consolas', 30, True)
@@ -39,29 +40,24 @@ while True:
 		if x.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
-		if x.type == pygame.MOUSEBUTTONDOWN:
-			Snipe.play()
-			print (mx, my)
-			if (mx>posx and mx<posx+200) and (my>posy and my<posy+150):
-				score+=1
-				prey_Y=None
-				posx=randint(0,800)
-				posy=randint(0,850)
+			
+		
 		if x.type == pygame.USEREVENT and timecounter!="Time's up": 
-			timecounter -= 1
+			movecount+=1
+			if movecount +1  >= 4:
+				movecount = 0
+			screen.blit(prey[movecount], (posX,posY))
+			pygame.display.update()
+			timecounter -= 0.25
 			texttime = str(timecounter)
 			if timecounter < 0:
 				timecounter="Time's up"
 				continue
 		elif timecounter == "Time's up":
-			textscore = None
-			fomtscore = None
-			texttime = None
-			fomttime = None
 			screen.blit(bg_END, (0,0))
 			pygame.display.update()
 			textscore1 = fontscore.render(' ' + str(timecounter), True, (0,255,255))
-			fomtscore1 = pygame.font.SysFont('consolas', 100, True)
+			fomtscore1 = pygame.font.SysFont('consolas', 300, True)
 			texttime1 = fontscore.render('Final Score:' + str(score), True, (0,255,255))
 			fomttime1 = pygame.font.SysFont('consolas', 120, True)
 			screen.blit(textscore1, (450, 450))
@@ -70,6 +66,15 @@ while True:
 			time.sleep(5)
 			pygame.quit()
 			sys.exit()
+		if x.type == pygame.MOUSEBUTTONDOWN:
+			Snipe.play()
+			print (mx, my)
+			if (mx>posX and mx<posX+190) and (my>posY and my<posY+190):
+				score+=1
+				prey=None
+				posX=randint(0,810)
+				posY=randint(0,810)
+		
 
 			
 
